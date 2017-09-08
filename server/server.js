66a7862831cb78cb5da961083ae954d9edd213de
Michael Luo <mluo@Michaels-MacBook-Pro.local>
2017-09-08 13:08:14 +1000

    enbale heroku port

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 import express from './config/express';
 // import mongoose from './config/mongoose';
 import mongoose from "mongoose";
@@ -11,8 +9,9 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.db.uri, config.db.options).then(connection => {
-    app.listen(config.server.port, () => {
+    app.listen(process.env.PORT || config.server.port, () => {
         // console.log('App started on port ' + config.server.port);
+        console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
     });
 }).catch(error => {
     console.log('ERROR:', error);
