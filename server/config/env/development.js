163dd5c6a26aba8cffa377e89c07f92eb42d1bc8
Michael Luo <michael.luo0801@gmail.com>
2018-08-07 23:11:55 +1000

    update

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,17 +1,15 @@
 // Invoke 'strict' JavaScript mode
-'use strict';
+"use strict";
 
 // Set the 'development' environment configuration object
 module.exports = {
-	server: {
-		port: 8080
-	},
-	db: {
-		uri: 'mongodb://localhost:27018/EMS',
-		// uri: 'mongodb://michael:michael@ds163053.mlab.com:63053/ems-mern',
-		// uri: 'mongodb://admin:<PASSWORD>@ems-shard-00-00-n24oi.mongodb.net:27017,ems-shard-00-01-n24oi.mongodb.net:27017,ems-shard-00-02-n24oi.mongodb.net:27017/test?ssl=true&replicaSet=ems-shard-0&authSource=admin',
-		options: {
-			useMongoClient: true
-		}
-	}
+  server: {
+    port: 8080
+  },
+  db: {
+    uri: "mongodb://localhost:27018/EMS",
+    options: {
+      useMongoClient: true
+    }
+  }
 };
